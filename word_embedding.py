eb496476d8d31c04f8f6a8c6f3ad3ed2fea6820c
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2019-01-01 01:31:04 +0330

    implement word2vec using gensim

--- word_embedding.py
+++ word_embedding.py
@@ -1,38 +1,49 @@
-from __future__ import unicode_literals
-
+import collections
+import datetime
 import re
-import string
 from hazm import *
+
 
 # str = '(۱۲): احمد[] پورمندى- اقدامى‌ها شجاعانه،'
 # print(word_tokenize(str))
-# print(list(filter(lambda s: s != '', re.compile('[ \'"،():\-_.$,\[\]!؟\n\t]').split(str))))
-# s = '2	۰۰۰ ميليارد تومان افزايش خواهد يافت.'
-# print(s.index('\t'))
-# d = s[s.index('\t')+1:]
-# print(d)
 # t = '۲۰۰'
 # print(list(map(int, re.findall(r'\d+', t))))
 FILE_NAME = 'data/fas_newscrawl_2017_100K/fas_newscrawl_2017_100K-sentences.txt'
-
 normalizer = Normalizer()
+
+
+def my_normalizer(content):
+    for i in range(len(content)):
+        sentence = content[i]
+        # remove number of sentence and \t
+        sentence = sentence[sentence.index('\t')+1:]
+        # hazm normalizer
+        normalizer.normalize(sentence)
+        # my tokenizer
+        sentence = list(filter(lambda s: s != '', re.compile('[ /\'"،؛ء–«»():\-_.$,\[\]!؟\n\t]').split(sentence)))
+        content[i] = sentence
+    content = [token for sentence in content for token in sentence]
+    return content
+
 
 def read_input(input_file):
     with open(input_file, encoding='utf-8') as f:
         content = f.readlines()
-        # remove number of sentence and \t
-        content = [sentence[sentence.index('\t')+1:] for sentence in content]
-        # hazm normalizer
-        content = [normalizer.normalize(sentence) for sentence in content]
-        # my tokenizer
-        content = [list(filter(lambda s: s != '', re.compile('[ \'"،():\-_.$,\[\]!؟\n\t]').split(sentence)))
-                   for sentence in content]
-        # make all numbers english
-        # content = [[list(map(int, re.findall(r'\d+', token))) for token in sentence] for sentence in content]
-
-        return content
+        tokens = my_normalizer(content)
+        return tokens
 
 
-documents = read_input(FILE_NAME)
-print(documents)
+begin = datetime.datetime.now()
+document = read_input(FILE_NAME)
+end = datetime.datetime.now()
 
+with open('tokens.txt', 'w') as f:
+    for i in range(len(document)):
+        print(document[i], end=", ", file=f),
+        if (i+1) % 30 == 0:
+            print(file=f)
+
+# in 100K sentences we have about 107K unique words (total words are about 2 millions)
+unique_words = list(set(document))
+
+print((end - begin).total_seconds())
